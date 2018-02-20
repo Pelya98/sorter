e0f82b6c0cf59546c3d351f571dfb017b6b1234c
Егор Пилипенко <egor.pilipenko.1998@gmail.com>
2018-02-21 01:01:37 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,32 +1,50 @@
 class Sorter {
-  constructor() {
- this.mass=[];
+    constructor() {
+        this.mass = [];
 
-  }
+    }
 
-  add(element) {
-this.mass.push(element);
-  }
+    add(element) {
+        this.mass.push(element);
+    }
 
-  at(index) {
-return this.mass[index];
-  }
+    at(index) {
+        return this.mass[index];
+    }
 
-  get length() {
-return this.mass.length;
+    get length() {
+        return this.mass.length;
 
-  }
+    }
 
-  toArray() {
-return this.mass;
-  }
+    toArray() {
+        return this.mass;
+    }
 
-  sort(indices) {
+    sort(indices) {
+        let res_mass = [];
+        for (let i = 0; i < indices.length; i++) {
+            res_mass.push(this.mass[indices[i]]);
+        }
+        res_mass.sort(function (a, b) {
+            return a - b;
+        });
 
-  }
+        if (indices[0] < indices[1])
+            for (let i = 0; i < indices.length; i++) {
+                this.mass.splice(indices[i], 1, res_mass[i])
+            }
+        else
+            indices.reverse();
+        for (let i = 0; i < indices.length; i++) {
+            this.mass.splice(indices[i], 1, res_mass[i])
 
-  setComparator(compareFunction) {
+        }
+    }
 
-  }
+    setComparator(compareFunction) {
+
+
+    }
 }
 module.exports = Sorter;
